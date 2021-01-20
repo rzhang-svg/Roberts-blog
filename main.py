b0ac94d972f38bd87159ce6130e6111b5853b651
Robert <rzhang1@berkeley.edu>
2021-01-19 18:30:40 -0800

    Deploy not working reverting some changes

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ def admin_only(f):
     return decorated_function
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get["SECRET_KEY", '8BYkEfBA6O6donzWlSihBXox7C0sKR6b']
+app.config['SECRET_KEY'] = os.environ.get["SECRET_KEY"]
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
