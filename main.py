ca6894a279ecc314334e456b5c601ee3ba296092
Robert <rzhang1@berkeley.edu>
2021-01-19 18:45:53 -0800

    Swapping to os.get secret Key

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def admin_only(f):
     return decorated_function
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
