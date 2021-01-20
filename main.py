4b66f7cdff536f549c20c99daf643249bd05d62a
Robert <rzhang1@berkeley.edu>
2021-01-19 18:28:31 -0800

    Added psycopg2-binary to include PostgreSQL integration

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from forms import CreatePostForm,  RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import os
 
 
 #Create admin-only decorator
@@ -24,12 +25,12 @@ def admin_only(f):
     return decorated_function
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get["SECRET_KEY", '8BYkEfBA6O6donzWlSihBXox7C0sKR6b']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
